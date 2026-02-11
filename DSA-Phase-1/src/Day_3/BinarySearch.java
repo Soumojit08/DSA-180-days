325b0465137abf70c8404ddb53213996c3d8efa8
soumojit banerjee <soumojitbanerjee08@gmail.com>
2026-02-12 00:45:10 +0530

    Binary Search

--- DSA-Phase-1/src/Day_3/BinarySearch.java
+++ DSA-Phase-1/src/Day_3/BinarySearch.java
@@ -4,7 +4,8 @@ import java.util.Scanner;
 public class BinarySearch {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        int[] arr = {1,2,3,4,5};
+        int[] arr = {1, 2, 3, 4, 5};
+        int[] arr2 = {5, 4, 3, 2, 1};
 
         System.out.println("Enter the target : ");
         int target = sc.nextInt();
@@ -13,6 +14,32 @@ public class BinarySearch {
             System.out.println("Elem not found in array ");
         }
         System.out.println("Elem " + target + " found at index " + ans);
+
+        int ansDesc = binarySearchDesc(arr2, target);
+        if (ansDesc == -1){
+            System.out.println("Elem not found in array ");
+        }
+        System.out.println("Elem " + target + " found at index " + ansDesc);
+        int ansAgonistic = binarySearchOrderAgonistic(arr, target);
+        if (ansAgonistic == -1){
+            System.out.println("Elem not found in array ");
+        }
+        System.out.println("Elem " + target + " found at index " + ansAgonistic);
+    }
+
+
+    public static int binarySearchOrderAgonistic(int[] arr, int target){
+        int start = 0;
+        int end = arr.length - 1;
+
+        if (arr[start] <= arr[end]){
+            binarySearch(arr, target);
+        }
+        else {
+            binarySearchDesc(arr, target);
+        }
+
+        return -1;
     }
 
     public static int binarySearch(int[] arr, int target){
@@ -28,6 +55,25 @@ public class BinarySearch {
                 start = mid+1;
             } else if (arr[mid] > target){
                 end = mid -1;
+            }
+
+        }
+        return -1;
+    }
+
+    public static int binarySearchDesc(int[] arr, int target){
+        int start = 0;
+        int end = arr.length - 1;
+
+        while (start <= end){
+            int mid = start + (end - start)/2;
+
+            if (arr[mid] == target){
+                return mid;
+            } else if (arr[mid] < target) {
+                end = mid - 1;
+            } else if (arr[mid] > target){
+                start = mid + 1;
             }
 
         }
