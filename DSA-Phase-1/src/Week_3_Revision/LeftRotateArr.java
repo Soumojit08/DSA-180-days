712dafb82c9f58ca2351fa66b87dfa8a9744e82e
soumojit banerjee <soumojitbanerjee08@gmail.com>
2026-04-13 11:07:58 +0530

    Left Rotate Array

--- DSA-Phase-1/src/Week_3_Revision/LeftRotateArr.java
+++ DSA-Phase-1/src/Week_3_Revision/LeftRotateArr.java
@@ -4,9 +4,11 @@ import java.util.Arrays;
 
 public class LeftRotateArr {
     public static void main(String[] args) {
-        int[] arr = {1, 2, 3, 4, 5};
+        int[] arr = {1, 2, 3, 4, 5, 6, 7};
         //output - 2,3,4,5,1 just move the first elem to end
-        byOnePlace(arr);
+//        byOnePlace(arr);
+        int k = 3;
+        byKPlace(arr, k);
     }
 
     public static void byOnePlace(int[] arr) { //O(N)
@@ -15,8 +17,31 @@ public class LeftRotateArr {
         for (int i = 1; i < arr.length; i++) {
             arr[i - 1] = arr[i];
         }
-        arr[arr.length-1]=temp;
+        arr[arr.length - 1] = temp;
 
         System.out.println(Arrays.toString(arr));
+    }
+
+    public static void byKPlace(int[] arr, int k) {
+        //Reverse the array 3 times to get ans
+        int n = arr.length;
+        k = k % n;
+
+        //reverse entire array
+        reverseArray(arr, 0, n - 1);
+        reverseArray(arr, 0, k - 1);
+        reverseArray(arr, k, n - 1);
+
+        System.out.println(Arrays.toString(arr));
+    }
+
+    private static void reverseArray(int[] arr, int start, int end) {
+        while (end > start) {
+            int temp = arr[start];
+            arr[start] = arr[end];
+            arr[end] = temp;
+            start++;
+            end--;
+        }
     }
 }
