b855ffd2ef112dc1e283f1d4cc8e3511e93b52f2
soumojit banerjee <soumojitbanerjee08@gmail.com>
2026-05-06 00:53:32 +0530

    Linkedlist creation

---
+++ DSA-Phase-2/src/LinkedList_1/Main.java
@@ -0,0 +1,10 @@
+package LinkedList_1;
+
+public class Main {
+    public static void main(String[] args) {
+        LL list = new LL();
+        list.inserFirst(10);
+
+        System.out.println(list);
+    }
+}
